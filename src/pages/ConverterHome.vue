619291fa0f153e3364292b6e1e4f4dfba5132efe
mfelipegs <matheusg.dasilva7@gmail.com>
2025-03-29 09:25:01 -0300

    feat: store setup on converterhome

--- src/pages/ConverterHome.vue
+++ src/pages/ConverterHome.vue
@@ -6,8 +6,27 @@
 <script>
 import UIHeader from "../components/UI/Header.vue";
 import MainConverterHome from "./components/MainConverterHome.vue";
+import { useCurrencyStore } from "@/stores/useCurrencyStore";
 
 export default {
+  setup() {
+    const curStore = useCurrencyStore();
+
+    curStore.setExchangeData({
+      result: "success",
+      base_code: "USD",
+      conversion_rates: {
+        USD: 1,
+        AUD: 1.4817,
+        BGN: 1.7741,
+        CAD: 1.3168,
+        CHF: 0.9774,
+        CNY: 6.9454,
+      },
+    });
+
+    return {};
+  },
   name: "ConverterHome",
   components: {
     UIHeader,
